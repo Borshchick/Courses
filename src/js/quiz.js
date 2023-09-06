53bd4c0ebc071ca09128b90ac61822aa3f7e8ea8
Rostislav <spellmane21@gmail.com>
2023-09-07 00:25:20 +0300

    quiz finish logic

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,23 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let quiz_arr = document.querySelectorAll(".quiz-submit");
-  let quiz_blocks = document.querySelectorAll(".quiz-content");
-  let quiz_radio = document.querySelectorAll('input[type="radio"]');
+  const final = document.querySelector(".final");
+  const checkFinal = document.querySelector(".check-final");
+  const quiz_arr = document.querySelectorAll(".quiz-submit");
+  const quiz_blocks = document.querySelectorAll(".quiz-content");
+  const quiz_radio = document.querySelectorAll('input[type="radio"]');
   let value;
   const fetchData = () => {
-    console.log("fetchData")
     const inputs = document.querySelectorAll("input");
     const values = {};
-    inputs.forEach((input, index) => {
+    inputs.forEach((input) => {
       if (input.type !== "submit") {
         if (input.type === "radio") {
-            // console.log(input, input.checked)
-            // if(!values[input.name] && input.checked){
-            //     values[input.name] = input.checked
-
-            // }
-            if(input.checked){
-                values[input.name] = input.value ? +input.value % 2 !== 0 : false
-            }
+          if (input.checked) {
+            values[input.name] = input.id.startsWith("Yes");
+          }
         } else {
           values[input.name] = input.value;
         }
@@ -33,20 +29,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
   quiz_arr.forEach((item, index) => {
     item.addEventListener("click", function () {
-      console.log(index);
       if (value != null && value != undefined) {
-        let val_class = document.getElementById(`${value}`);
-        console.log(val_class, value);
+        const val_class = document.getElementById(`${value}`);
         quiz_blocks[index].classList.add("d-none");
         val_class.classList.remove("d-none");
-        console.log(index, quiz_blocks.length);
         value = null;
         return;
       }
       quiz_blocks[index].classList.add("d-none");
       quiz_blocks[index + 1].classList.remove("d-none");
-      if(index === quiz_arr.length-1){
-        fetchData()
+    });
+  });
+
+  final.addEventListener("click", () => {
+    fetchData();
+  });
+
+  const questionRadios = [];
+
+  quiz_radio.forEach((input) => {
+    if (input.name === "isQuestions") {
+      questionRadios.push(input);
+    }
+  });
+
+  checkFinal.addEventListener("click", () => {
+    questionRadios.forEach((input) => {
+      if (input.checked) {
+        const selectedInputValue = input.id.startsWith("Yes");
+        if (!selectedInputValue) {
+          fetchData();
+        }
       }
     });
   });
