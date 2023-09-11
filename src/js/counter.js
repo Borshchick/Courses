7f9404ed3d79e011b778735d952a095714dbc798
Denis <Borschik>
2023-09-11 17:33:44 +0300

    counter animation fixed

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,31 +1,41 @@
+
 function formatNumberWithCommas(number) {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
-function animateValue(element, start, end, duration) {
-  const range = end - start;
-  const startTime = performance.now();
-
-  function updateNumber(currentTime) {
-    const elapsed = currentTime - startTime;
-    const progress = Math.min(elapsed / duration, 1);
-    const value = progress * range + start;
-
-    element.textContent = formatNumberWithCommas(value.toFixed(1));
-
-    if (progress < 1) {
-      requestAnimationFrame(updateNumber);
-    }
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   }
-
-  requestAnimationFrame(updateNumber);
-}
-
-const counterElement = document.querySelectorAll(".counter");
-
-counterElement.forEach((elem) => {
-  const endValue = elem.getAttribute("data-end");
-  const duration = 2000;
-
-  animateValue(elem, 0, +endValue, duration);
-});
+  
+  function animateValue(element, start, end, duration) {
+    const range = end - start;
+    const startTime = performance.now();
+  
+    function updateNumber(currentTime) {
+      const elapsed = currentTime - startTime;
+      const progress = Math.min(elapsed / duration, 1);
+      const value = progress * range + start;
+  
+      element.textContent = formatNumberWithCommas(value.toFixed(1));
+  
+      if (progress < 1) {
+        requestAnimationFrame(updateNumber);
+      }
+    }
+  
+    requestAnimationFrame(updateNumber);
+  }
+  
+  const counterElements = document.querySelectorAll(".counter");
+  
+  counterElements.forEach((elem) => {
+    const endValue = parseFloat(elem.getAttribute("data-end"));
+    const duration = 2000;
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(elem); 
+          animateValue(elem, 0, endValue, duration);
+        }
+      });
+    });
+    observer.observe(elem);
+  });
+  
